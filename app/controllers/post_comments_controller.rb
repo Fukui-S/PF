f5feec5e3cc22546bcf8597e40ddbfc73e2821f1
Fukui-S <tibita13579@yahoo.co.jp>
2021-06-18 06:30:22 +0000

    いいね機能追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,14 +2,15 @@ class PostCommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.post_comment.new(post_comment_params)
-    comment.post_id = post_image.id
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_id = post.id
     comment.save
     redirect_to post_path(post)
   end
 
   def destroy
-
+    #PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # redirect_to post_path(post)
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_105004) do
+ActiveRecord::Schema.define(version: 2021_06_18_060844) do
 
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "post_comments", force: :cascade do |t|
