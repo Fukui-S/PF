0730cad4bb5e8de8cdac3a7fe59a2666f89d049b
Fukui-S <tibita13579@yahoo.co.jp>
2021-06-19 11:34:59 +0000

    user/show追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @post = @user.post.page(params[:page]).reverse_order
 
   end
 
   def edit
+    @user = User.find(params[:id])
 
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :post, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  attachment :user_image
 end
